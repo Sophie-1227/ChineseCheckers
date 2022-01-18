d7f60f0e08809293a607ec4820796817af471760
Sophie-1227 <92611357+Sophie-1227@users.noreply.github.com>
2022-01-18 19:51:38 +0100

    Update Pionek.java
    Javadoc v1

--- Plansza_V2/src/main/java/Pionek.java
+++ Plansza_V2/src/main/java/Pionek.java
@@ -3,17 +3,29 @@ import java.awt.*;
 
 public class Pionek {
 	
+	/**
+	*Klasa zawiera metody pozwalające na obsługę pionka
+	*/
+	
 	private Color color;
 	
 	public Pionek(Color color) {
 		 this.color = color;
 	}
 	
+	/**
+	*Kopiowanie pionka
+	*/
+	
 	public Pionek kopiujPionek() {
 		Color color = this.getColor();
 		Pionek pionek = new Pionek(color);
 		return pionek;
 	}
+	
+	/**
+	*Pobieranie koloru pionka
+	*/
 	
 	public Color getColor() {
 		return this.color;
